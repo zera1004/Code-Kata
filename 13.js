535d10504fc1f03c07132948f448da04013bafef
김지웅 <sizera1004@gmail.com>
2024-11-08 11:10:17 +0900

    자릿수 더하기

---
+++ 13.js
@@ -0,0 +1,12 @@
+function solution(n)
+{
+    let temp = String(n);
+    let result = 0;
+    for (i = 0; i < temp.length; i++) {
+        result += Number(temp[i]);
+    }
+    return result;
+}
+
+// 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+// 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
